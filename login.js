7724cac09240bd3ca3e1796a386b89f6ee2db5a3
Datrix001 <mayankcollege01@gmail.com>
2024-03-10 14:09:12 +0530

    Initial Commit

---
+++ login.js
@@ -0,0 +1,20 @@
+const content1 = document.querySelector(".content1");
+const content2 = document.querySelector(".content2");
+
+document.querySelector('#sd').addEventListener("click", () => {
+  if (document.querySelector('#sd').innerHTML === "Signup") {
+    document.querySelector('.content').classList.add("active1");
+    setTimeout(() => {
+      content1.style.display = "none";
+    content2.style.display = "block";
+    document.querySelector('#sd').innerHTML = "Login";
+    },1900);
+  } else {
+    document.querySelector('.content').classList.remove("active1");
+    setTimeout(() => {
+    content1.style.display = "block";
+    content2.style.display = "none";
+    document.querySelector('#sd').innerHTML = "Signup";
+    },1900);
+  }
+});
---
+++ profile.php
@@ -0,0 +1,88 @@
+<?php
+    
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    
+    
+    
+    $con  = mysqli_connect($server,$username,$password);
+    session_start();
+    if(!$con){
+        die("Connection to this database failed due to ".mysqli_connect_error());
+    }
+?>
+
+
+<!-- Linging favicon -->
+<link rel="icon" href="css/cook-book.ico">
+
+<!-- Linking Bootstrap -->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="css/profile.css">
+<!-- Linking Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+    href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+    rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anta&display=swap" rel="stylesheet">
+<title>FlavorForge</title>
+</head>
+
+<body>
+    <?php
+        $unique_Id = $_SESSION['UNIQUE_ID'];
+        $sql = "SELECT * FROM `logintable`.`reg` WHERE  UNIQUE_ID = '{$_SESSION['UNIQUE_ID']}'";
+        $sql1 = mysqli_query($con,$sql);
+        $row = mysqli_fetch_assoc($sql1);
+    ?>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg p-3 ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"> <span><img src="images/chef-hat.ico" alt=""> FlavorForge</span></a>
+            <ul class="navbar-nav ms-lg-auto pe-xxl-3 ">
+                <li class="nav-item px-2 ">
+                    <a class="nav-link active" href="">Home</a>
+                </li>
+                <li class="nav-item px-2 ">
+                    <a class="nav-link active" href="">Recipes</a>
+                </li>
+                <li class="nav-item  px-2 ">
+                    <a class="nav-link active" href="">Contact Us</a>
+                </li>
+                <li class="nav-item  px-2 mt-2">
+                    <img src="images/profile.png" alt="" class="profile">
+                </li>
+                <li class="nav-item px-2 mt-2 ">
+                    <b class="size"><?php  echo $row['Name']; ?></b>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class = "container">
+        <h1 class="h1">Personal Information</h1>
+        <img src="images/user.png" alt="" class = "img1">
+        <form action="" method ="post"  class = "forum">
+            Name <br><input type="text" placeholder="<?php echo $row ['Name']; ?>" disabled><br>
+            Date of Birth <br><input type="date" ><br>
+            Email <br><input type="text" placeholder="<?php echo $row ['Email']; ?>" disabled><br>
+            About <br> <input type="text" name="about" id="about" ><br>
+            <input type="submit" class= "button">
+
+        </form>
+    </div>
+
+</body>
+</html>
